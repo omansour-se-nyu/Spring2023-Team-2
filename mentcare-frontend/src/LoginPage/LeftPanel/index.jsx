8fad097fee1a92e7f1e2fb9c1513209f739f43f7
swittuthcoding <swittuthcoding@gmail.com>
2023-02-20 23:14:32 -0500

    created context to manage state of application and udpate state to handle changing of logging in for users

---
+++ mentcare-frontend/src/LoginPage/LeftPanel/index.jsx
@@ -0,0 +1,56 @@
+import { Flex, Stack, Button } from '@chakra-ui/react';
+import MentcareLogo from '../../assets/MentcareLogo';
+import { AppContext } from '../../context/AppContext';
+import { useContext } from 'react';
+
+const LeftPanel = () => {
+  const { isAdmin, setIsAdmin, isStaff, setIsStaff } = useContext(AppContext);
+
+  const handleClickUser = (e) => {
+    const user = e.target.innerText;
+    if (user === 'staff') {
+      setIsStaff((value) => true);
+      setIsAdmin((value) => false);
+    } else if (user === 'admin') {
+      setIsStaff((value) => false);
+      setIsAdmin((value) => true);
+    }
+  };
+
+  return (
+    <Flex
+      height='100%'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='center'
+    >
+      <MentcareLogo />
+      <Stack direction='row' gap='20px'>
+        <Button
+          borderRadius='80px'
+          width='100px'
+          backgroundColor='#EC408B'
+          color='#FFFFFF'
+          opacity={isStaff ? '100%' : '50%'}
+          fontweight='medium'
+          onClick={handleClickUser}
+        >
+          staff
+        </Button>
+        <Button
+          borderRadius='80px'
+          width='100px'
+          backgroundColor='#EC408B'
+          color='#FFFFFF'
+          opacity={isAdmin ? '100%' : '50%'}
+          fontweight='medium'
+          onClick={handleClickUser}
+        >
+          admin
+        </Button>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default LeftPanel;
