28682f6ab520b9ef5e91c21f04f75213504ef7ea
swittuthcoding <swittuthcoding@gmail.com>
2023-02-20 22:29:29 -0500

    created component and design for sign in form on right panel

--- mentcare-frontend/src/LoginPage/RightPanel/Login.jsx
+++ mentcare-frontend/src/LoginPage/RightPanel/Login.jsx
@@ -1,15 +1,63 @@
-import { Flex } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
-import { Input } from '@chakra-ui/react';
+import { FormControl, Button, Input, Link, Text } from '@chakra-ui/react';
 
 const Login = () => {
   return (
-    <Flex flexDirection='column' alignItems={'center'} gap='10px'>
-      <h1 style={{ fontSize: '1.5em' }}>SIGN IN</h1>
-      <Input placeholder='enter username' size='md' />
-      <Input placeholder='enter password' size='md' />
-      <Button>Login</Button>
-    </Flex>
+    <form>
+      <FormControl
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          minWidth: '300px',
+          maxWidth: '500px',
+          width: '100%',
+          alignItems: 'center',
+          gap: '10px',
+        }}
+      >
+        <Text fontSize='3em' fontWeight='bold' color='#FB5058'>
+          SIGN IN
+        </Text>
+        <Input
+          placeholder='enter username'
+          size='md'
+          type='email'
+          width='100%'
+          borderRadius='80px'
+          backgroundColor='#F3EED9'
+          autoComplete='off'
+          _focusVisible={{
+            outlineColor: '#F3EED9',
+          }}
+        />
+        <Input
+          placeholder='enter password'
+          size='md'
+          type='password'
+          borderRadius='80px'
+          backgroundColor='#F3EED9'
+          auto
+          autoComplete='off'
+          _focusVisible={{
+            outlineColor: '#F3EED9',
+          }}
+        />
+        <Button
+          borderRadius='80px'
+          minWidth='200px'
+          backgroundColor='#FB5058'
+          color='#FFFFFF'
+          _hover={{
+            opacity: '70%',
+          }}
+          _active={{
+            opacity: '90%',
+          }}
+        >
+          Login
+        </Button>
+        <Link color='#FB5058'>Forgot password?</Link>
+      </FormControl>
+    </form>
   );
 };
 
