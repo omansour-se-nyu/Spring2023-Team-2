4b622ce130259e0173c4dba53e4ddbffc6986d1e
swittuthcoding <swittuthcoding@gmail.com>
2023-02-20 23:03:53 -0500

    created context state to manage state for the application

--- mentcare-frontend/src/App.jsx
+++ mentcare-frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { AppContext } from './context/AppContext';
+import { useState } from 'react';
 import LoginPage from './LoginPage';
 
 function App() {
+  const [isAdmin, setIsAdmin] = useState(true);
+  const [isStaff, setIsStaff] = useState(false);
+
   return (
     <ChakraProvider>
-      <LoginPage />
+      <AppContext.Provider value={{ isAdmin, setIsAdmin, isStaff, setIsStaff }}>
+        <LoginPage />
+      </AppContext.Provider>
     </ChakraProvider>
   );
 }
