4fc7425c47df260462c5978616ad38e32227b9bf
swittuthcoding <swittuthcoding@gmail.com>
2023-02-24 18:53:18 -0500

    udpated styling for the admin login page

--- mentcare-frontend/src/AdminPage/index.jsx
+++ mentcare-frontend/src/AdminPage/index.jsx
@@ -1,5 +1,26 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
 const AdminPage = () => {
-  return <div>This is the placeholder for the Admins's page</div>;
+  return (
+    <Grid templateColumns='repeat(12, 1fr)' height='100%'>
+      <GridItem
+        style={{
+          border: '1px solid red',
+        }}
+        colSpan={3}
+      >
+        Menu
+      </GridItem>
+      <GridItem
+        style={{
+          border: '1px solid blue',
+        }}
+        colSpan={9}
+      >
+        Dashboard
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default AdminPage;
