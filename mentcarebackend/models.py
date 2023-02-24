13a5ebfc77fbcb3eec8e523a62d5fbef877e3ebc
Margaret Jagger <margaretjagger2@gmail.com>
2023-02-24 11:03:56 -0500

    Add basic CRUD functionality

--- mentcarebackend/models.py
+++ mentcarebackend/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 # Create your models here.
 class MentcareModel(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.CharField(max_length=100)
-    is_completed = models.BooleanField(default=False)
-    date_created = models.DateField(auto_created=True)
-    last_modified = models.DateField(auto_now=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    date_hired = models.DateField(auto_created=True)
+    email = models.CharField(auto_now=True)
 
     def __str___(self):
         return self.title
--- mentcarebackend/views.py
+++ mentcarebackend/views.py
@@ -17,21 +17,25 @@ class ListMentcareAPIView(ListAPIView):
     """This endpoint list all of the available items from the database"""
     queryset = MentcareModel.objects.all()
     serializer_class = MentcareSerializer
+    http_method_names = ["get"]
 
 
 class CreateMentcareAPIView(CreateAPIView):
     """This endpoint allows for creation of a item"""
     queryset = MentcareModel.objects.all()
     serializer_class = MentcareSerializer
+    http_method_names = ["post"]
 
 
 class UpdateMentcareAPIView(UpdateAPIView):
     """This endpoint allows for updating a specific item"""
     queryset = MentcareModel.objects.all()
     serializer_class = MentcareSerializer
+    http_method_names = ["put"]
 
 
 class DeleteMentcareAPIView(DestroyAPIView):
     """This endpoint allows for deletion of a specific item from the database"""
     queryset = MentcareModel.objects.all()
     serializer_class = MentcareSerializer
+    http_method_names = ["delete"]
