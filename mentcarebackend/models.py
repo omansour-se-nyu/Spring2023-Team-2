da390b33f0d2ebda234f3ba05a3c2d77bfc48914
Margaret Jagger <margaretjagger2@gmail.com>
2023-02-26 20:07:30 -0500

    Bug fix

--- mentcarebackend/models.py
+++ mentcarebackend/models.py
@@ -6,7 +6,7 @@ class MentcareModel(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     date_hired = models.DateField(auto_created=True)
-    email = models.CharField(auto_now=True)
+    email = models.CharField(max_length=100)
 
     def __str___(self):
         return self.title
