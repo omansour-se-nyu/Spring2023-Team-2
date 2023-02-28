9345e63ec463edbec72b29d8582fc3135773dc59
iamaryaak <kulkarya@gmail.com>
2023-02-28 10:27:27 -0500

    reset LoginPage in App.jsx

--- mentcare-frontend/src/NoAccess/RightPanel/NoAccess.jsx
+++ mentcare-frontend/src/NoAccess/RightPanel/NoAccess.jsx
@@ -1,20 +1,26 @@
 import { FormControl, Button, Input, Link, Text } from '@chakra-ui/react';
-import Login from '../../LoginPage';
+import LoginPage from '../../LoginPage';
+import { useNavigate } from "react-router-dom";
 
 const NoAccess = () => {
   // fix Page Linking for "go back" button
+  let navigate = useNavigate();
+  const handleClickUser = (e) => {
+    let path = LoginPage;
+    navigate(path);
+  };
   return (
         <div>
         <Text fontSize='3em' fontWeight='bold' color='#FB5058' style={{justifyContent:'center'}}>
           No access
         </Text>
 
-       <Link to='/LoginPage'>
        <Button
           borderRadius='80px'
           minWidth='200px'
           backgroundColor='#FB5058'
           color='#FFFFFF'
+          onClick={handleClickUser}
           style={{marginLeft:'10px'}}
           _hover={{
             opacity: '70%',
@@ -24,7 +30,7 @@ const NoAccess = () => {
           }}
         >
           Go Back
-        </Button></Link>
+        </Button>
        </div>
   );
 };
