25d06bc32548cef97e4fc24d84c51338f58e684e
Margaret Jagger <margaretjagger2@gmail.com>
2023-03-03 20:20:08 -0500

    Refactor name of Logins table

--- mentcarebackend/serializers.py
+++ mentcarebackend/serializers.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from mentcarebackend.models import MentcareModel
+from mentcarebackend.models import MentcareModel, MentcareLoginsModel
 
 
 class MentcareSerializer(serializers.ModelSerializer):
     class Meta:
         model = MentcareModel
+        fields = "__all__"
+
+
+class MentcareLoginsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MentcareLoginsModel
         fields = "__all__"
