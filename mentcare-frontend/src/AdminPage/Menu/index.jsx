d2eb48570186d154c24917b776acb0534b511363
swittuthcoding <swittuthcoding@gmail.com>
2023-03-03 22:21:30 -0500

    updated title for admin page and installed react-router-dom

--- mentcare-frontend/src/AdminPage/Menu/index.jsx
+++ mentcare-frontend/src/AdminPage/Menu/index.jsx
@@ -1,9 +1,20 @@
+import { HStack, Text } from '@chakra-ui/react';
+
 const Menu = () => {
   return (
-    <nav>
-      This is the nav menu
-      <div>placehodler</div>
-    </nav>
+    <>
+      <HStack padding='10px'>
+        <p>Image</p>
+        <Text color='#FB5058' fontSize='4xl' fontWeight='bold'>
+          admin
+        </Text>
+      </HStack>
+
+      <nav>
+        This is the nav menu
+        <div>placehodler</div>
+      </nav>
+    </>
   );
 };
 
