c5ef2c1209ad6d1d1338ba456a043bd24eb3c8d3
Margaret Jagger <margaretjagger2@gmail.com>
2023-03-03 20:52:03 -0500

    Use Token authentication

--- mentcare/settings.py
+++ mentcare/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from rest_framework.schemas import coreapi
 
+import mentcarebackend.models
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -47,8 +49,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
 }
 
