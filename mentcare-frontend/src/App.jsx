3f7e90398fead3232d122558f27bfa570bc8cb97
swittuthcoding <swittuthcoding@gmail.com>
2023-03-03 21:34:57 -0500

    removed Admin Page

--- mentcare-frontend/src/App.jsx
+++ mentcare-frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AppContext } from './context/AppContext';
 import { useState } from 'react';
 import LoginPage from './LoginPage';
-import AdminPage from './AdminPage';
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(true);
@@ -11,8 +10,7 @@ function App() {
   return (
     <ChakraProvider>
       <AppContext.Provider value={{ isAdmin, setIsAdmin, isStaff, setIsStaff }}>
-        {/* <LoginPage /> */}
-        <AdminPage />
+        <LoginPage />
       </AppContext.Provider>
     </ChakraProvider>
   );
