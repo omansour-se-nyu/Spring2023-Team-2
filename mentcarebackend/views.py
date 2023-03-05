c840ca299016fd525fa927c22bfdb2d14dc10781
Margaret Jagger <margaretjagger2@gmail.com>
2023-03-05 15:54:29 -0500

    Add create patient record functionality

--- mentcarebackend/views.py
+++ mentcarebackend/views.py
@@ -10,21 +10,11 @@ from rest_framework.generics import DestroyAPIView
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 
-from mentcarebackend.models import MentcareModel, MentcareLoginsModel
-from mentcarebackend.serializers import MentcareSerializer, MentcareLoginsSerializer
+from mentcarebackend.models import *
 
 
 # Create your views here.
-
-
-class UserAuthentication(UpdateAPIView):
-    model = MentcareLoginsModel
-
-    def get_user(self, queryset=None):
-        return MentcareLoginsModel.objects.get(user=self.request.user)
-
 
 @csrf_exempt
 def login_view(request):
@@ -61,64 +51,55 @@ def login_view(request):
                              'code': status.HTTP_400_BAD_REQUEST})
 
 
-# class AuthenticatedView(APIView):
-# permission_classes = [IsAuthenticated, ]
-
-# def get(self, request):
-#     if request.user.is_authenticated:
-#         return Response(status.HTTP_200_OK)
-#     else:
-#         return Response(status.HTTP_403_FORBIDDEN)
+@csrf_exempt
+# todo: add logout functionality
+def logout_user(request):
+    pass
 
 
-class ListMentcareLoginsAPIView(APIView):
-    permission_classes = IsAuthenticated
-    queryset = MentcareLoginsModel.objects.all()
-    serializer_class = MentcareLoginsSerializer
-
-    # def login(request):
-    #     username = request.POST['username']
-    #     password = request.POST['password']
-    #     user = authenticate(username=username, password=password)
-    #
-    #     if user is not None:
-    #         login(request, user)
-    #     else:
-    #         Response(status.HTTP_403_FORBIDDEN)
-    #
-    # @permission_classes([IsAuthenticated])
-    # def get(self, request):
-    #     content = {
-    #         'user': str(request.user),  # `django.contrib.auth.User` instance.
-    #         'password': str(request.auth),  # None
-    #     }
-    #     return Response(content)
+@csrf_exempt
+# todo: add user registration functionality
+def register_user(reqeust):
+    pass
 
 
-# Create your views here.
-class ListMentcareAPIView(ListAPIView):
-    """This endpoint list all the available items from the database"""
-    queryset = MentcareModel.objects.all()
-    serializer_class = MentcareSerializer
-    http_method_names = ["get"]
+@csrf_exempt
+def create_patient_records(request):
+    if request.method == 'POST':
+        try:
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            gender = request.POST.get('gender')
+            dob = request.POST.get('dob')
+            address = request.POST.get('address')
+            phone_num = request.POST.get('phone_num')
 
+            # test all fields are present
+            if (first_name is None or last_name is None or gender is None or dob is None
+                    or address is None or phone_num is None):
+                return JsonResponse({'status': 'Error',
+                                     'message': 'Missing field. Cannot create patient record',
+                                     'code': status.HTTP_400_BAD_REQUEST})
 
-class CreateMentcareAPIView(CreateAPIView):
-    """This endpoint allows for creation of a item"""
-    queryset = MentcareModel.objects.all()
-    serializer_class = MentcareSerializer
-    http_method_names = ["post"]
+            else:
+                record = PatientInformationModel.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    gender=gender,
+                    dob=dob,
+                    address=address,
+                    phone_num=phone_num
+                )
 
+                record.save()
 
-class UpdateMentcareAPIView(UpdateAPIView):
-    """This endpoint allows for updating a specific item"""
-    queryset = MentcareModel.objects.all()
-    serializer_class = MentcareSerializer
-    http_method_names = ["put"]
+                return JsonResponse({'status': 'Success',
+                                     'message': 'Patient record created successfully',
+                                     'code': status.HTTP_200_OK})
 
-
-class DeleteMentcareAPIView(DestroyAPIView):
-    """This endpoint allows for deletion of a specific item from the database"""
-    queryset = MentcareModel.objects.all()
-    serializer_class = MentcareSerializer
-    http_method_names = ["delete"]
+        except (json.JSONDecodeError, JSONDecodeError):
+            return JsonResponse({'status': 'Error', 'message': 'No username or password given',
+                                 'code': status.HTTP_400_BAD_REQUEST})
+    else:
+        return JsonResponse({'status': 'Error', 'message': 'Invalid request method',
+                             'code': status.HTTP_400_BAD_REQUEST})
