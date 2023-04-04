5636d1e2c06bd5132c00c3133c7a0be82388f149
iamaryaak <kulkarya@gmail.com>
2023-04-04 16:12:35 -0400

    editing staff page

--- mentcare-frontend/src/StaffPage/Overview/OverviewDisplay/index.jsx
+++ mentcare-frontend/src/StaffPage/Overview/OverviewDisplay/index.jsx
@@ -2,9 +2,6 @@ import { Grid, GridItem } from '@chakra-ui/react';
 import CardSelection from './CardSelection';
 import { v4 as uuid } from 'uuid';
 import patientIcon from '../../../assets/patientIcon.png';
-import messages from '../../../assets/messages.png';
-import medicationsIcon from '../../../assets/medicationsIcon.png';
-import documents from '../../../assets/documents.png';
 import database from '../../../assets/database.png';
 import logout from '../../../assets/logout.png';
 
@@ -13,16 +10,13 @@ const OverviewDisplay = () => {
   const cardTitleImages = [
     // import images link when it is imported later
     ['Patients', patientIcon],
-    ['Messages', messages],
-    ['Medications', medicationsIcon],
-    ['Documents', documents],
     ['Database', database],
     ['Logout', logout],
 
   ];
 
   return (
-    <Grid templateColumns='repeat(3, 1fr)' gap='5' padding='30px' height='100%'>
+    <Grid templateColumns='repeat(2, 1fr)' gap='5' padding='30px' height='100%'>
       {cardTitleImages.map(([title, imageLink]) => (
         <GridItem colSpan={1} key={uuid()}>
           <CardSelection title={title} imageLink={imageLink} />
