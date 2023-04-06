b8a772691e4fe9721475190b34a097bfdafd80e5
Margaret Jagger <margaretjagger2@gmail.com>
2023-04-05 21:36:13 -0400

    Refactor imports

--- mentcare/urls.py
+++ mentcare/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework.documentation import include_docs_urls
 
 from mentcarebackend import views
 
