b5df5d7a7bb6d45e4fac781026e95d5275d03092
iamaryaak <kulkarya@gmail.com>
2023-04-24 16:08:34 -0400

    added patient perscriptions

--- mentcare-frontend/src/StaffPage/Overview/OverviewDisplay/MonthlyReport.jsx
+++ mentcare-frontend/src/StaffPage/Overview/OverviewDisplay/MonthlyReport.jsx
@@ -1,5 +1,8 @@
 import React, { useState , useEffect } from 'react';
 import {
+    VStack,
+    HStack,
+    Divider,
     InputGroup,
     InputLeftElement,
     Text,
@@ -27,7 +30,8 @@ import {
     FormLabel,
     ChakraProvider,
     Card,
-    CardBody
+    CardBody,
+    InputLeftAddon,
 } from '@chakra-ui/react';
 import {
   Chart as ChartJS,
@@ -85,16 +89,133 @@ const MonthlyReport = () => {
       ],
     };
 
+    const [medication, setMedication] = useState([]);
+    const fetchMedication = () => {
+        fetch('http://127.0.0.1:8000/staff/patients/drugs-prescribed/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ "month": "01", "year":"2022" }),
+        })
+          .then((response) => response.json())
+          .then((actualData) => {
+            const data = JSON.parse(actualData.medication_info);
+            console.log(data);
+            // all info
+            data.sort((a, b) => a.pk - b.pk);
+            setMedication(data);
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
+      };
+    useEffect(() => {
+       fetchMedication();
+    }, []);
+    // render table for Medication
+    const renderBody = () => {
+
+        if (medication.length === 0) return null;
+        return medication.map(({ pk, fields }) => {
+          const { medication_name, cost, application_method } = fields || {};
+          return (
+            <Tr key={pk}>
+              <Td>{pk}</Td>
+              <Td>{medication_name}</Td>
+              <Td>{cost}</Td>
+              <Td>{application_method}</Td>
+            </Tr>
+          );
+        });
+    };
+
+
     return(
         <ChakraProvider>
         <Text color='#FB5058' fontWeight='bold' fontSize='5xl' paddingLeft='30px'>
             Monthly Report
         </Text>
-        <Text textAlign='center' color='#FB5058' fontWeight='bold' fontSize='3xl' paddingLeft='30px'>
+        <Text color='#FB5058' fontWeight='bold' fontSize='3xl' paddingLeft='30px'>
             Monthly Patient Visits
         </Text>
         <Line options={options} data={data} />
-
+        <VStack height='100vh' width='100%'>
+      <VStack width='100%' height='100vh' align='start'>
+        <VStack
+          height='20vh'
+          minHeight='150px'
+          maxHeight='200px'
+          width='100%'
+          align='start'
+          paddingLeft='5px'
+          paddingRight='5px'
+        >
+          <Text color='#FB5058' fontWeight='bold' align='center' fontSize='4xl'>
+            Patient Overview
+          </Text>
+          <HStack width='100%' height='100%' justify='center'>
+            <InputGroup width='350px'>
+              <InputLeftAddon
+                children='Enter Month'
+                fontWeight='bold'
+                backgroundColor='#d5c37b'
+                color='#faf9ef'
+              />
+              <Input
+                type='text'
+                placeholder='eg. 123'
+                borderRadius='80px'
+                backgroundColor='#F3EED9'
+                focusBorderColor='#F3EED9'
+              />
+            </InputGroup>
+            <Divider height='30px' orientation='vertical' />
+            <InputGroup width='350px'>
+              <InputLeftAddon
+                children='Enter Year'
+                fontWeight='bold'
+                backgroundColor='#d5c37b'
+                color='#faf9ef'
+              />
+              <Input
+                type='text'
+                placeholder='eg. 123'
+                borderRadius='80px'
+                backgroundColor='#F3EED9'
+                focusBorderColor='#F3EED9'
+              />
+            </InputGroup>
+          </HStack>
+        </VStack>
+        <TableContainer height='80vh' width='100%' overflowY='auto'>
+          <Table size='sm' variant='striped'>
+            <Thead
+              style={{
+                position: 'sticky',
+                top: 0,
+                backgroundColor: '#FB5058',
+                zIndex: 'sticky',
+              }}
+            >
+            <Tr>
+            <Th fontSize='0.8em' color='white'>
+                <Text>Patient MRN</Text>
+            </Th>
+            <Th fontSize='0.8em' color='white'>
+                <Text>Medication</Text>
+            </Th>
+            <Th fontSize='0.8em' color='white'>
+                <Text>Cost</Text>
+            </Th>
+            <Th fontSize='0.8em' color='white'>
+                <Text>Application Method</Text>
+            </Th>
+            </Tr>
+            </Thead>
+            <Tbody>{renderBody()}</Tbody>
+          </Table>
+        </TableContainer>
+        </VStack>
+        </VStack>
         </ChakraProvider>
     );
 
